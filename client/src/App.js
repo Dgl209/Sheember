822dd13cded6b47c2495f59ce27fbc1063a1d846
dgl <mykulych15@gmail.com>
2022-05-02 15:44:16 +0300

    feat: create basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
+import React from 'react';
 import './App.css';
+import { Home, NotFound, Registration, Login } from './pages';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return null;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/registration" element={<Registration />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ client/src/pages/login/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div className="container mx-auto flex items-center justify-center h-screen">
+      <h1>Login page</h1>
+    </div>
+  );
+}
+
+export default Login;
